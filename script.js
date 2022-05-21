6f8ef5ff9ea0cfccca176ed0cfbd225594dca6bf
Andrey Laptev <andreilap1980@gmail.com>
2022-05-21 21:40:08 +0300

    feat: добавить стили CSS  добавить JS для бургера добавить

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const menuBtn = document.querySelector('.menu-btn');
+const page = document.querySelector('.page');
+const inputFooter = document.querySelectorAll('.input-footer');
+const menuBtnLinks = document.querySelector('.menu-btn__links');
+const menuBtnLink = document.querySelectorAll('.menu-btn__link');
+menuBtn.addEventListener('click', function () {
+  menuBtn.classList.toggle('active');
+  menuBtnLinks.classList.toggle('active');
+  page.classList.toggle('lock');
+})
 
+menuBtnLink.forEach(button => {
+  button.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menuBtnLinks.classList.toggle('active');
+    page.classList.toggle('lock');
+  })
+});
+
+inputFooter.forEach(button => {
+  button.addEventListener('click', function () {
+    page.classList.toggle('dark');
+  })
+});
